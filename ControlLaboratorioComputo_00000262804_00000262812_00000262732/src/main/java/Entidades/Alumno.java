84d12bba9e9143a21364e04a39730152937db5d3
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-06-11 08:11:17 -0600

    Clase Alumno
    Clase Alumno casi terminada.

--- ControlLaboratorioComputo_00000262804_00000262812_00000262732/src/main/java/Entidades/Alumno.java
+++ ControlLaboratorioComputo_00000262804_00000262812_00000262732/src/main/java/Entidades/Alumno.java
@@ -9,5 +9,72 @@ package Entidades;
  * @author BALAMRUSH
  */
 public class Alumno {
+    private Integer idAlumno;
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private boolean estatus;
+    private String contrasenia;
+
+    public Alumno() {
+    }
+
+    public Alumno(Integer idAlumno, String nombres, String apellidoPaterno, String apellidoMaterno, boolean estatus, String contrasenia) {
+        this.idAlumno = idAlumno;
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.estatus = estatus;
+        this.contrasenia = contrasenia;
+    }
+
+    public Integer getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(Integer idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public boolean isEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(boolean estatus) {
+        this.estatus = estatus;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    
     
 }
