184c535a5bd5bfeb0039a36d4bed1281854398e0
Andreiy Montoya Navarro <andreiymn02@gmail.com>
2026-06-13 02:21:32 -0700

    Correcion en sentencias y sintaxis

--- ControlLaboratorioComputo_00000262804_00000262812_00000262732/src/main/java/Persistencia/UnidadAcademicaDAO.java
+++ ControlLaboratorioComputo_00000262804_00000262812_00000262732/src/main/java/Persistencia/UnidadAcademicaDAO.java
@@ -17,6 +17,7 @@ import java.util.logging.Logger;
  *
  * @author Andre
  */
+/*
 public class UnidadAcademicaDAO implements IUnidadAcademicaDAO{
 
     private static final Logger LOGGER = Logger.getLogger(UnidadAcademicaDAO.class.getName());
@@ -79,5 +80,68 @@ public class UnidadAcademicaDAO implements IUnidadAcademicaDAO{
             throw new PersistenciaException("Error al consultar el listado de unidades académicas: " +ex.getMessage());
         }
     }
-    
+ */
+public class UnidadAcademicaDAO implements IUnidadAcademicaDAO {
+
+    private static final Logger LOGGER = Logger.getLogger(UnidadAcademicaDAO.class.getName());
+
+    private IConexionBD conexion;
+
+    public UnidadAcademicaDAO(IConexionBD conexion) {
+        this.conexion = conexion;
+    }
+
+    @Override
+    public UnidadAcademica consultarUnidadAcademicaPorID(Integer idUnidadAcademica) throws PersistenciaException {
+        try (Connection conexion = this.conexion.crearConexion()) {
+            String comandoSQL = """
+                    SELECT idUnidadAcademica,
+                           nombre
+                    FROM unidadAcademica
+                    WHERE idUnidadAcademica = ?
+                    """;
+            PreparedStatement statement = conexion.prepareStatement(comandoSQL);
+            statement.setInt(1, idUnidadAcademica);
+
+            ResultSet resultado = statement.executeQuery();
+            if (resultado.next()) {
+                return new UnidadAcademica(
+                        resultado.getInt("idUnidadAcademica"),
+                        resultado.getString("nombre"));
+            }
+            return null;
+
+        } catch (SQLException ex) {
+            LOGGER.severe(ex.getMessage());
+            throw new PersistenciaException("Error al consultar la Unidad Académica por ID: " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public List<UnidadAcademica> consultarUnidadesAcademicas(String filtro) throws PersistenciaException {
+        List<UnidadAcademica> listaUnidadesAcademicas = new ArrayList<>();
+        try (Connection conexion = this.conexion.crearConexion()) {
+            String comandoSQL = """
+                    SELECT idUnidadAcademica,
+                           nombre
+                    FROM unidadAcademica
+                    WHERE nombre LIKE ?
+                    """;
+            PreparedStatement statement = conexion.prepareStatement(comandoSQL);
+            String filtroBusqueda = "%" + filtro + "%";
+            statement.setString(1, filtroBusqueda);
+
+            ResultSet resultado = statement.executeQuery();
+            while (resultado.next()) {
+                listaUnidadesAcademicas.add(new UnidadAcademica(
+                        resultado.getInt("idUnidadAcademica"),
+                        resultado.getString("nombre")));
+            }
+            return listaUnidadesAcademicas;
+
+        } catch (SQLException ex) {
+            LOGGER.severe(ex.getMessage());
+            throw new PersistenciaException("Error al consultar el listado de unidades académicas: " + ex.getMessage());
+        }
+    }
 }
