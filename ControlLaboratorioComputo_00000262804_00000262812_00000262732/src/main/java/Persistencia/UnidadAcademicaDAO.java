67b9539bd682076e79e682dd303692648dbdf725
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-06-12 18:56:04 -0700

    Actualización UnidadAcademicaDAO

--- ControlLaboratorioComputo_00000262804_00000262812_00000262732/src/main/java/Persistencia/UnidadAcademicaDAO.java
+++ ControlLaboratorioComputo_00000262804_00000262812_00000262732/src/main/java/Persistencia/UnidadAcademicaDAO.java
@@ -4,10 +4,44 @@
  */
 package Persistencia;
 
+import Entidades.UnidadAcademica;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Andre
  */
-public class UnidadAcademicaDAO {
+public class UnidadAcademicaDAO implements IUnidadAcademicaDAO{
+
+    private static final Logger LOGGER = Logger.getLogger(UnidadAcademicaDAO.class.getName());
+    
+    private IConexionBD conexion;
+    
+    public UnidadAcademicaDAO(IConexionBD conexion){
+        this.conexion = conexion;
+    }
+
+    
+    @Override
+    public UnidadAcademica consultarUnidadAcademicaPorID(Integer idUndadAcademica) throws PersistenciaException {
+        try(Connection conexion = this.conexion.crearConexion()){
+            String comandoSQL = """
+                                SELECT(
+                                        )
+                                """;
+            
+        }catch(SQLException ex){
+            LOGGER.severe(ex.getMessage());
+            throw new PersistenciaException("Error al conusltar la Unidad Académica por ID" +ex.getMessage());
+        }
+    }
+
+    @Override
+    public List<UnidadAcademica> consultarUnidadesAcademicas() throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
