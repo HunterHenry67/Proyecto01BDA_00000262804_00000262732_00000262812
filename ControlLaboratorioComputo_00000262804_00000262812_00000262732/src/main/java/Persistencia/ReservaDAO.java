ee2009ef3849df4f41adeef89070b7e23d1c62e2
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-06-13 14:24:44 -0700

    ReservaDAO
    Creación de clases

--- ControlLaboratorioComputo_00000262804_00000262812_00000262732/src/main/java/Persistencia/ReservaDAO.java
+++ ControlLaboratorioComputo_00000262804_00000262812_00000262732/src/main/java/Persistencia/ReservaDAO.java
@@ -5,54 +5,132 @@
 package Persistencia;
 
 import Dtos.ReservaDTO;
+import Entidades.Reserva;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Andre
  */
 public class ReservaDAO implements IReservaDAO{
-    
-    private final ConexionBD conexion = new ConexionBD();
 
+    private static final Logger LOGGER = Logger.getLogger(ReservaDAO.class.getName());
+    
+    private IConexionBD conexion;
+    private Connection transaccion;
+    
+    public ReservaDAO(IConexionBD conexion){
+        this.conexion = conexion;
+    }
+    
     @Override
-    public ReservaDTO obtenerReservaActiva(int idComputadora) {
+    public Reserva obtenerReservaActiva(int idComputadora) {
         String comandoSQL = """
-            SELECT r.idReserva,
-                   r.fechaHoraApartado,
-                   r.fechaHoraInicio,
-                   r.fechaHoraFinal,
-                   r.tiempoUso,
-                   r.idAlumno,
-                   r.idComputadora,
-                   CONCAT(a.nombre, ' ', a.apellidoPaterno) AS alumnoNombre
-            FROM reserva r
+            
             JOIN alumno a ON r.idAlumno = a.idAlumno
             WHERE r.idComputadora = ?
               AND r.fechaHoraFinal IS NULL
             """;
         
-        try (Connection conn = conexion.crearConexion();
-             PreparedStatement ps = conn.prepareStatement(comandoSQL)) {
-            
-            ps.setInt(1, idComputadora);
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    ReservaDTO reserva = new ReservaDTO();
-                    reserva.setIdReserva(rs.getInt("idReserva"));
-                    reserva.setFechaHoraApartado(rs.getTimestamp("fechaHoraApartado"));
-                    reserva.setIdAlumno(rs.getInt("idAlumno")); 
-                    reserva.setIdComputadora(rs.getInt("idComputadora"));
-                    reserva.setNombreAlumno(rs.getString("alumnoNombre")); // Atributo auxiliar
-                    return reserva;
-                }
+        
+    }
+
+    @Override
+    public Reserva registrarReserva(Reserva reserva) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Reserva consultarResrevaActivaPorAlumno(int idAlumno) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Reserva consultarReservaActivaPorComputadora(int idComputadora) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Reserva> consultar(String filtro) throws PersistenciaException {
+        List<Reserva> listaFiltroReserva = new ArrayList<>();
+        try(Connection conexion = this.conexion.crearConexion()){
+            String comandoSQL = """
+                                        SELECT 
+                                        r.idReserva,
+                                        r.fechaHoraApartado,
+                                        r.fechaHoraInicio,
+                                        r.fechaHoraFinal,
+                                        r.tiempoUso,
+                                        r.idAlumno,
+                                        r.idComputadora,
+                                        CONCAT(a.nombre, ' ', a.apellidoPaterno, ' ', a.apellidoMaterno) AS alumnoNombre
+                                    FROM reserva r
+                                    INNER JOIN alumno a ON r.idAlumno = a.idAlumno
+                                    INNER JOIN computadora c ON r.idComputadora = c.idComputadora
+                                    WHERE DATE_FORMAT(r.fechaHoraApartado, '%Y-%m-%d %H:%i:%s') LIKE ?
+                                       OR DATE_FORMAT(r.fechaHoraInicio, '%Y-%m-%d %H:%i:%s') LIKE ?
+                                       OR DATE_FORMAT(r.fechaHoraFinal, '%Y-%m-%d %H:%i:%s') LIKE ?
+                                       OR CONCAT(a.nombre, ' ', a.apellidoPaterno, ' ', a.apellidoMaterno) LIKE ?
+                                       OR c.nombre LIKE ?;
+                                """;
+            PreparedStatement statement = conexion.prepareStatement(comandoSQL);
+            String busquedaFiltro = "%" + filtro + "%";
+            statement.setString(1, busquedaFiltro);
+            statement.setString(2, busquedaFiltro);
+            statement.setString(3, busquedaFiltro);
+            statement.setString(4, busquedaFiltro);
+            statement.setString(5, busquedaFiltro);
+            ResultSet resultado = statement.executeQuery();
+            while(resultado.next()){
+                Timestamp fechaInicio = resultado.getTimestamp("fechaHoraInicio");
+                Timestamp fechaFinal = resultado.getTimestamp("fechaHoraFinal");
+                listaFiltroReserva.add(new Reserva(resultado.getInt("idReserva"),
+                                                   resultado.getTimestamp("fechaHoraApartado").toLocalDateTime(),
+                                                   fechaInicio != null ? fechaInicio.toLocalDateTime(): null,
+                                                   fechaFinal != null ? fechaFinal.toLocalDateTime(): null,
+                                                   resultado.getObject("tiempoUso") != null ? resultado.getInt("tiempoUso"): null,
+                                                   resultado.getInt("idAlumno"),
+                                                   resultado.getInt("idComputadora")
+                                                   ));
             }
-        } catch (SQLException e) {
-            System.err.println("Error en ReservaDAO.obtenerReservaActiva: " + e.getMessage());
+            return listaFiltroReserva;
+        }catch(SQLException ex){
+            LOGGER.severe(ex.getMessage());
+            throw new PersistenciaException("Error al consultar las reservas por filtro: " + ex.getMessage());
         }
-        return null;
+    }
+
+    @Override
+    public List<Reserva> consultarReservasActivas() throws PersistenciaException {
+        List<Reserva> listaReservasActivas = new ArrayList<>();
+        try(Connection conexion = this.conexion.crearConexion()){
+            
+        }catch(SQLException ex){
+            LOGGER.severe(ex.getMessage());
+            throw new PersistenciaException("Error al consultar las reservas activas: "+ex.getMessage());
+        }
+    }
+
+    @Override
+    public void finalizarBloqueo(int idReserva, LocalDateTime fechaFinalizacion) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void cancelarBloqueo(int idReserva) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int consultarMinutosUsadosPorAlumno(int idAlumno) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
