64d4b1a3eadb757f0457ba5a89121e655554709e
increible <vortegalopez5@gmail.com>
2026-06-15 01:49:30 -0700

    main
    modificacion main pantalla principal

--- ControlLaboratorioComputo_00000262804_00000262812_00000262732/src/main/java/PresentacionProgramaApartado/frmPantallaPrincipal.java
+++ ControlLaboratorioComputo_00000262804_00000262812_00000262732/src/main/java/PresentacionProgramaApartado/frmPantallaPrincipal.java
@@ -10,6 +10,7 @@ import Negocio.IAlumnoBO;
 import Negocio.NegocioException;
 import Persistencia.AlumnoDAO;
 import Persistencia.ConexionBD;
+import Persistencia.PersistenciaException;
 import PresentacionProgramaApartado.PresentacionException;
 import javax.swing.JOptionPane;
 
@@ -173,6 +174,30 @@ public class frmPantallaPrincipal extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btnIngresarActionPerformed
 
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(frmPantallaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new frmPantallaPrincipal().setVisible(true);
+        });
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnIngresar;
     private javax.swing.JLabel lblFiltro;
